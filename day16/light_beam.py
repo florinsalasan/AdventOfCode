caa650a0597c5e4e654ffaef42eed4ad17434ce4
Florin Salasan <florinsalasan@gmail.com>
2024-01-05 12:51:14 -0500

    reworking some parts of generating the new indices

--- day16/light_beam.py
+++ day16/light_beam.py
@@ -28,13 +28,14 @@ WIDTH = len(lines[0])
 
 # Probably need a way to walk through the grid, swear there's been a past
 # puzzle that had a similar pathing through a grid.
-directions = {
-    # all directions are x, y
-    'L': [-1, 0],
-    'R': [1, 0],
-    'U': [0, -1],
-    'D': [0, 1]
-}
+# won't be accessing the directions from the dict, is kinda clunky to do so
+# directions = {
+# all directions are x, y
+#     'L': [-1, 0],
+#     'R': [1, 0],
+#     'U': [0, -1],
+#     'D': [0, 1]
+# }
 
 # create a dict that contains all of the coordinates of the
 # tiles that a lightbeam has passed through
@@ -54,25 +55,25 @@ def find_path(maze, start):
     seen = set()  # keep in mind this is unordered, but lets us not have to
     # worry about dupes
     while queue != []:
-        curr = queue[0]
-        queue = queue[1:]
-        curr_direction = directions[curr[-1]]
-        new_x = curr[0] + curr_direction[0]
-        new_y = curr[1] + curr_direction[1]
-        if new_x < 0 or new_x >= WIDTH or new_y < 0 or new_y >= HEIGHT:
-            # means we are inside the grid still so can add it.
+        # while the queue is not empty, check the first item
+        # and add any other necessary items to the queue
+        x, y, dx, dy = queue.pop(0)
+
+        # check if x, y are valid indices
+        if x < 0 or x >= WIDTH or y < 0 or y >= HEIGHT:
+            # out of bounds, try next value in queue
             continue
 
-        curr_char = maze[new_y][new_x]
+        new_x = x + dx
+        new_y = y + dy
 
-        if (curr_char == '.' or
-            (curr_char == '-' and curr_direction[1] != 0) or
-                (curr_char == '|' and curr_direction[0] != 0)):
-            queue.append((curr[0], curr[1], curr_direction))
-            seen.add(curr[0], curr[1], curr_direction)
+        seen.add((x, y, dx, dy))
 
-        # not looking forward to doing all of the different cases
-        # for where the path goes next.
+        curr_char = maze[y][x]
+
+        if curr_char == '.' or (curr_char == '-' and dy == 0) or (curr_char == '|' and dx == 0):
+            # continue in the same direction
+            queue.append((new_x, new_y, dx, dy))
 
 
 find_path(lines, (0, 0, 'R'))
