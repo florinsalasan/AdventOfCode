72ff7347148c9983c8405e38547fd8c15af0f82d
Florin Salasan <florinsalasan@gmail.com>
2024-01-07 13:40:22 -0500

    changed to iterative approach, still off by one in the test input so need to do a deeper check

--- day16/light_beam.py
+++ day16/light_beam.py
@@ -53,11 +53,15 @@ def find_path(maze, start):
     # the start position and items in queue should have x, y, then a direction
     # from the dict
     seen = set()  # keep in mind this is unordered, but lets us not have to
+    coords = set()
     # worry about dupes
     while queue != []:
         # while the queue is not empty, check the first item
         # and add any other necessary items to the queue
         x, y, dx, dy = queue.pop(0)
+
+        if (x, y, dx, dy) in seen:
+            continue
 
         # check if x, y are valid indices
         if x < 0 or x >= WIDTH or y < 0 or y >= HEIGHT:
@@ -68,6 +72,7 @@ def find_path(maze, start):
         # new_y = y + dy
 
         seen.add((x, y, dx, dy))
+        coords.add((x, y))
 
         curr_char = maze[y][x]
 
@@ -83,6 +88,26 @@ def find_path(maze, start):
             queue.append((x, y + 1, 0, 1))
             queue.append((x, y - 1, 0, -1))
         elif curr_char == '\\':
+            # need to check for dy both -1 and 1, and same for dx for
+            # all possible next nodes. Unless there is some way to
+            # do this more elegantly. The only way I can think of
+            # is setting dx to 0 if nonzero, then dy to dx and same
+            # for dy if dy is nonzero
+            if dy != 0:
+                queue.append((x + dy, y, dy, 0))
+            elif dx != 0:
+                queue.append((x, y + dx, 0, dx))
+        elif curr_char == '/':
+            # is setting dx to 0 if nonzero, then dy to -dx and same
+            # for dy if dy is nonzero
+            if dy != 0:
+                queue.append((x + (-dy), y, -dy, 0))
+            elif dx != 0:
+                queue.append((x, y + (-dx), 0, -dx))
+
+        print(curr_char)
+
+    print(coords)
 
 
-find_path(lines, (0, 0, 'R'))
+find_path(lines, (0, 0, 1, 0))
