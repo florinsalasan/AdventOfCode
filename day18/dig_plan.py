441c98fbe08f5e0268921d89054495a5d90d0240
Florin Salasan <florinsalasan@gmail.com>
2024-01-15 12:39:56 -0500

    going to take some stuff from day10, but should be a lot simpler, just need the edges i think

--- day18/dig_plan.py
+++ day18/dig_plan.py
@@ -12,10 +12,124 @@ if lines[-1] == '' or lines[-1] == '\n':
 print(lines)
 
 max_up = 0
-curr_up = 0
 max_down = 0
-curr_down = 0
+curr_up_down = 0
 max_left = 0
-curr_left = 0
 max_right = 0
-curr_right = 0
+curr_left_right = 0
+
+
+def move_up(count, curr_up_down):
+    curr_up_down -= count
+    return curr_up_down
+
+
+def move_down(count, curr_up_down):
+    curr_up_down += count
+    return curr_up_down
+
+
+def move_left(count, curr_left_right):
+    curr_left_right -= count
+    return curr_left_right
+
+
+def move_right(count, curr_left_right):
+    curr_left_right += count
+    return curr_left_right
+
+
+def draw_up(count, curr_up_down):
+    new_curr = curr_up_down + count
+    return new_curr
+
+
+def draw_down(count, curr_up_down):
+    new_curr = curr_up_down + count
+    return new_curr
+
+
+def draw_left(count, curr_left_right):
+    new_curr = curr_left_right + count
+    return new_curr
+
+
+def draw_right(count, curr_left_right):
+    new_curr = curr_left_right + count
+    return new_curr
+
+
+for line in lines:
+    dir, count, colour = line.split(' ')
+    count = int(count)
+    dir = dir.lower()
+    if dir == 'u':
+        new_curr = move_up(count, curr_up_down)
+        curr_up_down = new_curr
+        if curr_up_down < max_up:
+            max_up = curr_up_down
+
+    elif dir == 'd':
+        new_curr = move_down(count, curr_up_down)
+        curr_up_down = new_curr
+        if curr_up_down > max_down:
+            max_down = curr_up_down
+
+    elif dir == 'r':
+        new_curr = move_right(count, curr_left_right)
+        curr_left_right = new_curr
+        if curr_left_right > max_right:
+            max_right = curr_left_right
+
+    elif dir == 'l':
+        new_curr = move_left(count, curr_left_right)
+        curr_left_right = new_curr
+        if curr_left_right < max_left:
+            max_left = curr_left_right
+
+print(max_left, max_up, max_right, max_down)
+
+WIDTH = abs(max_left - max_right)
+HEIGHT = abs(max_up - max_down)
+
+print(WIDTH, HEIGHT)
+
+# create the grid, and a path like we've done before, then count enclosed
+# spaces like we've also done before
+
+# If you start the path at -max_left, -max_right, should all fit in a grid
+# of WIDTH, HEIGHT as defined above.
+
+grid = [list('.'*WIDTH)] * HEIGHT
+
+curr_left_right = -max_left
+curr_up_down = -max_up
+
+for line in lines:
+    dir, count, colour = line.split(' ')
+    count = int(count)
+    dir = dir.lower()
+
+    if dir == 'u':
+        new_curr = draw_up(count, curr_up_down)
+        curr_up_down = new_curr
+        if curr_up_down < max_up:
+            max_up = curr_up_down
+
+    elif dir == 'd':
+        new_curr = draw_down(count, curr_up_down)
+        curr_up_down = new_curr
+        if curr_up_down > max_down:
+            max_down = curr_up_down
+
+    elif dir == 'r':
+        new_curr = draw_right(count, curr_left_right)
+        curr_left_right = new_curr
+        if curr_left_right > max_right:
+            max_right = curr_left_right
+
+    elif dir == 'l':
+        new_curr = draw_left(count, curr_left_right)
+        curr_left_right = new_curr
+        if curr_left_right < max_left:
+            max_left = curr_left_right
