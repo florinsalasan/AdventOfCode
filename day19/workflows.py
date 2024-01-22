fa58b9cd7350160404e6cd96a1c5adac72fb8ffa
Florin Salasan <florinsalasan@gmail.com>
2024-01-22 15:19:09 -0500

    made progress on the classes to parse the input text

--- day19/workflows.py
+++ day19/workflows.py
@@ -17,14 +17,36 @@ parts = all_lines[all_lines.index('\n') + 1:]
 class decision_node:
 
     def __init__(self, input_str):
-        pieces = list(input_str)
         name = input_str[:input_str.index('{')]
         rules = input_str[input_str.index('{') + 1: input_str.index('}')]
-        print(pieces, name, rules)
+        rules = ''.join(rules)
+        rules = rules.split(',')
+
+        classed_rules = []
+        for rule_idx, rule in enumerate(rules):
+            classed_rules.append((rule_idx, ruleset(rule)))
+
+        self.rules = classed_rules
+        self.name = name
+
+
+class ruleset:
+
+    def __init__(self, input_str):
+        if ':' not in input_str:
+            self.result = input_str
+            self.prop = None
+            self.prop_value = None
+            self.prop_comp = None
+            return
+        comp_idx = max(input_str.find('<'), input_str.find('>'))
+        colon_idx = input_str.find(':')
+        self.result = input_str[colon_idx + 1:]
+        self.prop = input_str[:comp_idx]
+        self.prop_value = input_str[comp_idx + 1:colon_idx]
+        self.prop_comp = input_str[comp_idx]
 
 
 noded_workflows = []
 for workflow in workflows:
     noded_workflows.append(decision_node(workflow))
-
-print(noded_workflows)
