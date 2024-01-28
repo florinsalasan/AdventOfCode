746eb93215ddd98959a58532d3ad459a77b99643
Florin Salasan <florinsalasan@gmail.com>
2024-01-28 11:57:46 -0500

    started creating the factory pattern

--- day20/product_pulses.py
+++ day20/product_pulses.py
@@ -7,10 +7,24 @@ with open(sys.argv[1]) as input_file:
     lines = input_file.readlines()
 
 
+class flip_flop:
+
+    def __init__(self, input_str):
+        self.input_str = input_str
+        print('flip flopped')
+
+
 class generic_module:
 
     def __init__(self, input_str):
         self.input_str = input_str
+        items = input_str.split(' ')
+        newline_removed = items[-1][0:-1]
+        items[-1] = newline_removed
+        prefix = items[0][0]
+        self.creation = None
+        if prefix == '%':
+            self.creation = flip_flop(input_str)
 
 
 modules = {}
@@ -23,3 +37,6 @@ for line in lines:
     # generic_module will be a factory class that places the line in the proper
     # module subclass
     new_module = generic_module(line)
+    print('new module: ' + line)
+    print(type(new_module))
+    print(type(new_module.creation))
