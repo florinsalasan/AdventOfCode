ec33da3f141cdca284f7b93dae18c172401c43d6
Florin Salasan <florinsalasan@gmail.com>
2024-12-11 13:16:16 -0500

    finished pt2.py, added the position of each antenna assuming that they will be antinode locations too, unsuree if the requirements was asking for that but I could not parse what the rule was exactly

--- 2024advent/day08/pt2.py
+++ 2024advent/day08/pt2.py
@@ -18,3 +18,47 @@ for y_idx, line in enumerate(lines):
                 antenna_and_their_positions[value].append((x_idx, y_idx))
             else:
                 antenna_and_their_positions[value] = [(x_idx, y_idx)]
+
+
+def find_antinodes(antenna_and_their_positions):
+    antinode_positions = set()
+    # go through each frequency, find any valid antinode positions and throw
+    # them into a set, return len of the set
+    for freq in antenna_and_their_positions.keys():
+        antennas = antenna_and_their_positions[freq]
+        for i in range(len(antennas)):
+            antinode_positions.add((antennas[i][0], antennas[i][1]))
+            for j in range(i + 1, len(antennas)):
+                diff_x = antennas[i][0] - antennas[j][0]
+                diff_y = antennas[i][1] - antennas[j][1]
+
+                if diff_y == 0:
+                    print("same y_idx")
+                if diff_x == 0:
+                    print("same x_idx")
+
+                behind_i_x = antennas[i][0] + diff_x
+                behind_i_y = antennas[i][1] + diff_y
+
+                ahead_j_x = antennas[j][0] - diff_x
+                ahead_j_y = antennas[j][1] - diff_y
+
+                while (behind_i_x >= 0 and behind_i_x < WIDTH and
+                        behind_i_y >= 0 and behind_i_y < HEIGHT):
+                    antinode_positions.add((behind_i_x, behind_i_y))
+                    behind_i_x += diff_x
+                    behind_i_y += diff_y
+
+                while (ahead_j_x >= 0 and ahead_j_x < WIDTH and
+                        ahead_j_y >= 0 and ahead_j_y < HEIGHT):
+                    antinode_positions.add((ahead_j_x, ahead_j_y))
+                    ahead_j_x -= diff_x
+                    ahead_j_y -= diff_y
+
+    for position in antinode_positions:
+        print(position)
+
+    print(len(antinode_positions))
+
+
+find_antinodes(antenna_and_their_positions)
